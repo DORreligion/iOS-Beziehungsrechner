f12299da54cab498e08df225a1b4adf93b36cdad
Lukas Schramm <schramm98@icloud.com>
2015-11-28 13:42:09 +0100

    Programm ist mit grundlegendem Design ausführbar.

---
+++ Beziehungsrechner/Calculater.swift
@@ -0,0 +1,85 @@
+//
+//  Calculater.swift
+//  Beziehungsrechner
+//
+//  Created by Lukas Schramm on 30.10.15.
+//  Copyright © 2015 Dabendorf. All rights reserved.
+//
+
+import Foundation
+
+class Calculate {
+    
+    var names = [String]()
+    var numbers = [Int]()
+    var relationRate = Int()
+    var length = Int()
+    var temp = Int()
+    var counter = Int()
+    
+    init(names:[String]) {
+        self.names = names
+        sort()
+        calc()
+    }
+    
+    func sort() {
+        names = names.sort()
+        let nameStr = (names[0] + names[1]).uppercaseString
+        let chars = Array(nameStr.characters)
+        counter = 0
+        for a in chars {
+            for b in chars {
+                if(a==b) {
+                    counter++
+                }
+            }
+            numbers.append(counter)
+            counter = 0
+        }
+        print(numbers)
+    }
+    
+    func calc() {
+        var numbersStatic = [Int]()
+        var digits = [Int]()
+        for number in numbers {
+            numbersStatic.append(number)
+        }
+        length = numbersStatic.count
+        temp = length-1
+        numbers.removeAll()
+        
+        for var i=0;i<(length/2);i++ {
+            counter = numbersStatic[i] + numbersStatic[temp]
+            if counter > 9 {
+                while counter > 0 {
+                    digits.insert(counter%10, atIndex: 0)
+                    counter/=10
+                }
+                numbers.append(digits[0])
+                numbers.append(digits[1])
+            } else {
+                numbers.append(counter)
+            }
+            temp--;
+        }
+        
+        if length%2 != 0 {
+            numbers.append(numbersStatic[length/2])
+        }
+        if numbers.count > 2 && !(numbers.count==3 && numbers[0]==1 && numbers[1]==0 && numbers[2]==0) {
+            calc()
+        } else {
+            var str = ""
+            for number in numbers {
+                str += "\(number)"
+            }
+            relationRate = Int(str)!
+        }
+    }
+
+    func getRelationRate() -> Int {
+        return relationRate
+    }
+}
--- Beziehungsrechner/ViewController.swift
+++ Beziehungsrechner/ViewController.swift
@@ -8,11 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var getName1: UITextField!
+    @IBOutlet weak var getName2: UITextField!
+    @IBOutlet weak var labelErgebnis: UILabel!
+    
+    @IBAction func calculateButton(sender: UIButton) {
+        generateRelation()
+    }
+    
+    func generateRelation() {
+        var names = [String]()
+        var complete = true
+        if let name1 = getName1.text {
+            names.append(name1)
+        } else {
+            complete = false
+        }
+        
+        if let name2 = getName2.text {
+            names.append(name2)
+        } else {
+            complete = false
+        }
+        
+        if names[0]=="" || names[1]=="" {
+            complete = false
+        }
+        
+        if complete {
+            let calc = Calculate(names: names)
+            labelErgebnis.text = "Beziehungsrate: \(calc.getRelationRate()) %"
+        } else {
+            labelErgebnis.text = "Es fehlen Namen!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.getName1.delegate = self
+        self.getName2.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +56,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        generateRelation()
+        return false
+    }
 }
-
